42094dca8bf60e652953511c70506a01d09e3b51
Alisson <acm91997@gmail.com>
2024-03-02 19:12:11 -0500

    perfeccionando el HeaderFiltro

--- src/components/Empleados/HeaderFiltro.jsx
+++ src/components/Empleados/HeaderFiltro.jsx
@@ -1,32 +1,132 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import '../../style/HeaderStyle.css';
 
 
 export function HeaderFiltro() {
 
-  const [isOpen, setIsOpen] = useState(false);
+  const [areaMenuOpen, setAreaMenuOpen] = useState(false);
+  const [cargoMenuOpen, setCargoMenuOpen] = useState(false);
+  const [salarioMenuOpen, setSalarioMenuOpen] = useState(false);
+  const [ubicacionMenuOpen, setUbicacionMenuOpen] = useState(false);
 
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
+
+  const areaRef = useRef(null);
+  const cargoRef = useRef(null);
+  const salarioRef = useRef(null);
+  const ubicacionRef = useRef(null);
+
+  useEffect(() => {
+    const handleOutsideClick = (event) => {
+      if (
+        areaRef.current &&
+        !areaRef.current.contains(event.target) &&
+        cargoRef.current &&
+        !cargoRef.current.contains(event.target) &&
+        salarioRef.current &&
+        !salarioRef.current.contains(event.target) &&
+        ubicacionRef.current &&
+        !ubicacionRef.current.contains(event.target)
+      ) {
+        setAreaMenuOpen(false);
+        setCargoMenuOpen(false);
+        setSalarioMenuOpen(false);
+        setUbicacionMenuOpen(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleOutsideClick);
+
+    return () => {
+      document.removeEventListener('mousedown', handleOutsideClick);
+    };
+  }, []);
+
+  const toggleAreaMenu = () => {
+    setAreaMenuOpen(!areaMenuOpen);
+    setCargoMenuOpen(false);
+    setSalarioMenuOpen(false);
+    setUbicacionMenuOpen(false);
+  };
+
+  const toggleCargoMenu = () => {
+    setCargoMenuOpen(!cargoMenuOpen);
+    setAreaMenuOpen(false);
+    setSalarioMenuOpen(false);
+    setUbicacionMenuOpen(false);
+  };
+
+  const toggleSalarioMenu = () => {
+    setSalarioMenuOpen(!salarioMenuOpen);
+    setAreaMenuOpen(false);
+    setCargoMenuOpen(false);
+    setUbicacionMenuOpen(false);
+  };
+
+  const toggleUbicacionMenu = () => {
+    setUbicacionMenuOpen(!ubicacionMenuOpen);
+    setAreaMenuOpen(false);
+    setCargoMenuOpen(false);
+    setSalarioMenuOpen(false);
   };
 
     return(
       <div className='HeaderWork'>
         <ul className='nav'>
-          <li>
-          <button className={`nav-button ${isOpen ? 'isOpen' : ''}`} onClick={toggleMenu}>Área</button>
-          {isOpen && (
+          <li ref={areaRef}>
+          <button className='nav-button' onClick={toggleAreaMenu}>
+            Área
+            <i className='fa fa-caret-down'></i>
+          </button>
+          {areaMenuOpen && (
             <ul className='dropdown-menu'>
               <li>Elemento 1</li>
               <li>Elemento 2</li>
               <li>Elemento 3</li>
             </ul>
           )}
-            
           </li>
-          <li><a href='#'>Cargo</a></li>
-          <li><a href='#'>Salario</a></li>
-          <li><a href='#'>Ubicación</a></li>
+
+          <li ref={cargoRef}>
+          <button className='nav-button' onClick={toggleCargoMenu}>
+            Cargo
+            <i className='fa fa-caret-down'></i>
+          </button>
+          {cargoMenuOpen && (
+            <ul className='dropdown-menu'>
+              <li>Elemento 1</li>
+              <li>Elemento 2</li>
+              <li>Elemento 3</li>
+            </ul>
+          )}
+          </li>
+          
+          <li ref={salarioRef}>
+          <button className='nav-button' onClick={toggleSalarioMenu}>
+            Salario
+            <i className='fa fa-caret-down'></i>
+          </button>
+          {salarioMenuOpen && (
+            <ul className='dropdown-menu'>
+              <li>Elemento 1</li>
+              <li>Elemento 2</li>
+              <li>Elemento 3</li>
+            </ul>
+          )}
+          </li>
+          
+          <li ref={ubicacionRef}>
+          <button className='nav-button' onClick={toggleUbicacionMenu}>
+            Ubicación
+            <i className='fa fa-caret-down'></i>
+          </button>
+          {ubicacionMenuOpen && (
+            <ul className='dropdown-menu'>
+              <li>Elemento 1</li>
+              <li>Elemento 2</li>
+              <li>Elemento 3</li>
+            </ul>
+          )}
+          </li>
         </ul>
       </div>
     )
